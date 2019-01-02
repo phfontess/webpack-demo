ec7d61bd1aeb2e554c3bc738e3332cd142bc7407
Pedro Fontes <phfontess@gmail.com>
2019-01-02 16:49:14 -0200

    following webpack docs getting started section

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import _ from 'lodash';
 
 function component() {
     let element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
     return element;
 }
 
