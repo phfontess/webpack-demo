2d6fa586470cd9162a47b1ed7116d7bede356f6e
Pedro Fontes <phfontess@gmail.com>
2019-01-02 17:16:50 -0200

    multiple entry points config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import _ from 'lodash';
+import printMe from './print';
 
 function component() {
-    let element = document.createElement('div');
+    const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
     return element;
 }
